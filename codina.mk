052e0e4b3f8f21749ce56ff4591fef8e5771736b
Robert Rozic <rooobertinhooo@gmail.com>
2014-02-23 15:17:10 +0100

    Omni 4.4 initial bring up

--- codina.mk
+++ codina.mk
@@ -5,8 +5,8 @@ $(call inherit-product, device/samsung/u8500-common/common.mk)
 $(call inherit-product, vendor/samsung/u8500-common/codina/codina-vendor-blobs.mk)
 
 ifneq ($(TARGET_SCREEN_HEIGHT),800)
-# Call cm.mk because somehow it's not being called!
-$(call inherit-product, device/samsung/codina/cm.mk)
+# Call omni_codina.mk because somehow it's not being called!
+$(call inherit-product, device/samsung/codina/omni_codina.mk)
 endif
 
 LOCAL_PATH := device/samsung/codina
