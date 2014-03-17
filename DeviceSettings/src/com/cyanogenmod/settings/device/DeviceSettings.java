61bc18707672c3c6846f49ad32171a4a6edc12d5
zwliew <zhaoweiliew@gmail.com>
2014-03-17 22:48:49 +0800

    DeviceSettings: audio_preferences: Done adding all on/off ABBamp settings
    Not sure if there are more. Will continue adding tunables.

--- DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -52,6 +52,13 @@ public class DeviceSettings extends FragmentActivity {
 	public static final String KEY_ENABLE_ANAGAIN3 = "enable_anagain3";
 	public static final String KEY_ENABLE_HSLDIGGAIN = "enable_hsldiggain";
 	public static final String KEY_ENABLE_HSRDIGGAIN = "enable_hsrdiggain";
+	public static final String KEY_ENABLE_HSLOWPOW = "enable_hslowpow";
+	public static final String KEY_ENABLE_HSDACLOWPOW = "enable_hsdaclowpow";
+	public static final String KEY_ENABLE_HSHPEN = "enable_hshpen";
+	public static final String KEY_ENABLE_CLASSDHPG = "enable_classdhpg";
+	public static final String KEY_ENABLE_CLASSDWG = "enable_classdwg";
+	public static final String KEY_ENABLE_ADDIGGAIN2 = "enable_addiggain2";
+	public static final String KEY_ENABLE_EARDIGGAIN = "enable_eardiggain";
 
 	ViewPager mViewPager;
 	TabsAdapter mTabsAdapter;
