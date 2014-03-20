37201bce668509de5894ad1deef14d0b79473b42
zwliew <zhaoweiliew@gmail.com>
2014-03-21 01:44:07 +0800

    DeviceSettings: Add tweaking of threshold x/y values of Sweep2Wake

--- DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -48,6 +48,8 @@ public class DeviceSettings extends FragmentActivity {
 	public static final String KEY_MALI_L2MR = "mali_l2_mr";
 	public static final String KEY_MALI_PAM = "mali_pam";
 	public static final String KEY_USE_SWEEP2WAKE = "use_sweep2wake";
+	public static final String KEY_X_SWEEP2WAKE = "x_sweep2wake";
+	public static final String KEY_Y_SWEEP2WAKE = "y_sweep2wake";
 	public static final String KEY_USE_SPI_CRC = "use_spi_crc";
 	public static final String KEY_SWITCH_STORAGE = "switch_storage";
 	public static final String KEY_ENABLE_ANAGAIN3 = "enable_anagain3";
